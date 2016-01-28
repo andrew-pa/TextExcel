8cbf7f31da7607b89bbfa1c24ce284df87a3f22c
Tristan Hilbert <tchilbert@live.com>
2016-01-28 08:45:07 -0800

    I made a constructor!!!

--- src/pw/qxczv/TextExcel/Spreadsheet.java
+++ src/pw/qxczv/TextExcel/Spreadsheet.java
@@ -6,25 +6,35 @@ import pw.qxczv.TextExcel.Values.Value;
  * Created by s-apalmer on 1/27/2016.
  */
 public class Spreadsheet {
-    Value[/*columns*/][/*rows*/] cells;
-    
-    public Value valueAt(char c, int r) {
-    	return cells[65-c][r]; //unfortunate reality is that a Spreadsheet is a column-store
-    }
-    
-    public void print() {
-    	for(int r = 0; r < cells[0].length+1; ++r) {
-    		for(int c = 0; c < cells.length+1; ++c) {
-    			if(r == 0) {
-    				//print column names
-    			} else if(c == 0) {
-    				//print row names
-    			}
-    		}
-    	}
-    }
+	Value[/* columns */][/* rows */] cells;
+
+	public Spreadsheet(int col, int row) {
+
+		cells = new Value[col][row];
+
+	}
+
+	public Value valueAt(char c, int r) {
+		return cells[65 - c][r]; // unfortunate reality is that a Spreadsheet is
+									// a column-store
+	}
+
+	public void print() {
+		for (int r = 0; r < cells[0].length + 1; ++r) {
+			for (int c = 0; c < cells.length + 1; ++c) {
+				if (r == 0) {
+					// print column names
+				} else if (c == 0) {
+					// print row names
+				}
+			}
+		}
+	}
 
 	public void setValue(char c, int r, Value rv) {
-		cells[65-c][r] = rv; //sad reality is that sometimes you just need a setter, but these aren't so bad because they provide translation between col char names and indices
+		cells[65 - c][r] = rv; // sad reality is that sometimes you just need a
+								// setter, but these aren't so bad because they
+								// provide translation between col char names
+								// and indices
 	}
 }
