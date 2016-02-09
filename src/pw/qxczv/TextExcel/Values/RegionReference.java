482d99eaf0685818907374aa64f4b4be844bc194
Tristan Hilbert <tchilbert@live.com>
2016-02-08 20:19:49 -0800

    Created a new private method. Taking a break for now might reconvene at a later time to talk about spec.

--- src/pw/qxczv/TextExcel/Values/RegionReference.java
+++ src/pw/qxczv/TextExcel/Values/RegionReference.java
@@ -1,13 +1,30 @@
 package pw.qxczv.TextExcel.Values;
 
+import pw.qxczv.TextExcel.Spreadsheet;
+
 public class RegionReference extends Value{
 
-	public char colStrtIdx; public int rowStrtIdx; public int colRegRng; public int rowRegRng;
+	public char colStrtIdx; public int rowStrtIdx; public char colEndIdx; public int rowEndIdx;
 	
-	public RegionReference (char c, int r, int colEnd, int rowEnd){
+	public RegionReference (char c, int r, char colEnd, int rowEnd){
 		//colEnd and rowEnd are inclusive
-		colStrtIdx = c; rowStrtIdx = r; colRegRng = colEnd; rowRegRng = rowEnd;
+		colStrtIdx = c; rowStrtIdx = r; colEndIdx = colEnd; rowEndIdx = rowEnd;
 	}
+	
+	@Override
+	public Value resolve(Spreadsheet s) {
+		Value[] arry = getValueArray(s);
+	}
+	
+	public Value[] rawDeref(Spreadsheet s) {
+		return getValueArray(s);
+	}
+	
+	@Override
+	public void assign(Spreadsheet s, Value[]) {
+		
+	}
+	
 	
 	@Override
 	public int compareTo(Value o) {
@@ -17,7 +34,27 @@ public class RegionReference extends Value{
 	
 	@Override
 	public String toString(){
-		return colStrtIdx + "" + rowStrtIdx + " - " + ((char)(colRegRng + ((int) colStrtIdx))) + (rowStrtIdx + rowRegRng);
+		return colStrtIdx + "" + rowStrtIdx + " - " + colEndIdx + rowEndIdx;
+	}
+	
+	private Value[] getValueArray(Spreadsheet s){
+		Value[] arry = new Value[(((int)colEndIdx)-((int)colStrtIdx)) * (rowEndIdx - rowStrtIdx) + 1];
+		int i = 0;
+		Value temp;
+		//Yes I know this is super messy --- Feel free to clean up
+		for(int row = rowStrtIdx; row <= rowEndIdx; row ++){
+			for(int col = (int)colStrtIdx; col <= (int)colEndIdx; col ++){
+		//Also feel free to change the order of the nested loop
+				temp = s.valueAt((char)col, row);
+				if(temp != null){
+					arry[i] = temp;
+				}else{
+					arry[i] = null;
+				}
+				i++;
+			}
+		}
+		return arry;
 	}
 	
 }
