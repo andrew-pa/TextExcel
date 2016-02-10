55251778cd38c3b0fac5e992ca3b015a18a9ff3d
Tristan Hilbert <tchilbert@live.com>
2016-02-10 09:26:13 -0800

    Still working on

--- src/pw/qxczv/TextExcel/Values/RegionReference.java
+++ src/pw/qxczv/TextExcel/Values/RegionReference.java
@@ -2,69 +2,58 @@ package pw.qxczv.TextExcel.Values;
 
 import pw.qxczv.TextExcel.Spreadsheet;
 
-public class RegionReference extends LValue{
+public class RegionReference extends LValue {
 
-	public char colStrtIdx; public int rowStrtIdx; public char colEndIdx; public int rowEndIdx;
-	
-	public RegionReference (char c, int r, char colEnd, int rowEnd){
-		//colEnd and rowEnd are inclusive
-		colStrtIdx = c; rowStrtIdx = r; colEndIdx = colEnd; rowEndIdx = rowEnd;
+	public char colStrtIdx;
+	public int rowStrtIdx;
+	public char colEndIdx;
+	public int rowEndIdx;
+
+	public RegionReference(char c, int r, char colEnd, int rowEnd) {
+		// colEnd and rowEnd are inclusive
+		colStrtIdx = c;
+		rowStrtIdx = r;
+		colEndIdx = colEnd;
+		rowEndIdx = rowEnd;
 	}
-	
+
 	public Value[] rawDeref(Spreadsheet s) {
 		return getValueArray(s);
 	}
-	
-	@Override
-	public Value resolve(Spreadsheet s) {
-		//!!! THIS IS A CRAZZZZY HAX !!!
-		// This makes subtraction like ``` a1 - a2 ``` still work when evaluated in a context that doesn't require a region
-		// Functions that do require a region should avoid this by not resolving the value
-		// User-defined functions can be invoked with a region using deferred expressions, I think.. 
-		Value sv = s.valueAt(colStrtIdx, rowStrtIdx).resolve(s);
-		Value ev = s.valueAt(colEndIdx, rowEndIdx).resolve(s);
-		if(sv != null && ev != null && sv.getClass() == Number.class && ev.getClass() == Number.class) {
-			Number sn = (Number)sv; Number en = (Number)ev;
-			return new Number(sn.v - en.v);
-		} else {
-			return super.resolve(s);
-		}
-	}
-	
+
 	@Override
 	public void assign(Spreadsheet s, Value v) {
-		for(int row = rowStrtIdx; row <= rowEndIdx; row ++){
-			for(int col = (int)colStrtIdx; col <= (int)colEndIdx; col ++){
-				s.setValue((char)col, row, v);
-				//(sigh) yes I know I used a setter :'(
+		for (int row = rowStrtIdx; row <= rowEndIdx; row++) {
+			for (int col = (int) colStrtIdx; col <= (int) colEndIdx; col++) {
+				s.setValue((char) col, row, v);
+				// (sigh) yes I know I used a setter :'(
 			}
 		}
 	}
-	
-	
+
 	@Override
 	public int compareTo(Value o) {
 		// Andrew says that they will never be compared. RIP comparable Regions
 		return -1;
 	}
-	
+
 	@Override
-	public String toString(){
+	public String toString() {
 		return colStrtIdx + "" + rowStrtIdx + " - " + colEndIdx + rowEndIdx;
 	}
-	
-	private Value[] getValueArray(Spreadsheet s){
-		Value[] arry = new Value[(((int)colEndIdx)-((int)colStrtIdx)) * (rowEndIdx - rowStrtIdx) + 1];
+
+	private Value[] getValueArray(Spreadsheet s) {
+		Value[] arry = new Value[(((int) colEndIdx) - ((int) colStrtIdx)) * (rowEndIdx - rowStrtIdx) + 1];
 		int i = 0;
 		Value temp;
-		//Yes I know this is super messy --- Feel free to clean up
-		for(int row = rowStrtIdx; row <= rowEndIdx; row ++){
-			for(int col = (int)colStrtIdx; col <= (int)colEndIdx; col ++){
-		//Also feel free to change the order of the nested loop
-				temp = s.valueAt((char)col, row);
-				if(temp != null){
+		// Yes I know this is super messy --- Feel free to clean up
+		for (int row = rowStrtIdx; row <= rowEndIdx; row++) {
+			for (int col = (int) colStrtIdx; col <= (int) colEndIdx; col++) {
+				// Also feel free to change the order of the nested loop
+				temp = s.valueAt((char) col, row);
+				if (temp != null) {
 					arry[i] = temp;
-				}else{
+				} else {
 					arry[i] = null;
 				}
 				i++;
@@ -78,14 +67,16 @@ public class RegionReference extends LValue{
 		double returnedValue = 0;
 		Number tempNum;
 		Value tempResolvedValue;
-		for(Value v: arry){
+		for(int row = rowStrtIdx; row <= rowEndIdx; row ++){
+			for(int col = (int)colStrtIdx; col <= (int)colEndIdx; col ++){
 			tempResolvedValue = v.resolve(s);
 			if(tempResolvedValue != null && tempResolvedValue.getClass() == Number.class){
 				tempNum = (Number) tempResolvedValue;
 				returnedValue += tempNum.v;
 			}
+			}
 		}
-		return new Number(returnedValue);
-	}
-	
+		}return new Number(returnedValue);
+}
+
 }
