601a348b6156a12c280b28a36ec97bd891eb57f1
Tristan Hilbert <tchilbert@live.com>
2016-02-19 08:49:44 -0800

    File Input done... I think

--- src/pw/qxczv/TextExcel/FileStreams/SpreadsheetFileStream.java
+++ src/pw/qxczv/TextExcel/FileStreams/SpreadsheetFileStream.java
@@ -1,12 +1,43 @@
 package pw.qxczv.TextExcel.FileStreams;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+
 import pw.qxczv.TextExcel.Spreadsheet;
 
+
+
 public class SpreadsheetFileStream {
+	
+private static FileInputStream fileInput;
+private static ObjectInputStream ObjectInput;
 
 	public static Spreadsheet get(String name) {
-		// TODO Auto-generated method stub
+		try{
+			File saveFile = new File("/SavedSpreadsheets/" + name + ".qxczvSp");
+			fileInput = new FileInputStream(saveFile);
+			ObjectInput = new ObjectInputStream(fileInput);
+			return (Spreadsheet) ObjectInput.readObject();
+		}catch (IOException e){
+			//Change when ship
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			System.out.println("<Error--BadFile>");
+		}
 		return null;
+	}
+	
+	public static void closeCrap(){
+		try {
+			fileInput.close();
+			ObjectInput.close();
+		} catch (IOException e) {
+			//Change when ship
+			e.printStackTrace();
+		}
+		
 	}
 
 }
--- src/pw/qxczv/TextExcel/FileStreams/SpreadsheetFiles.java
+++ src/pw/qxczv/TextExcel/FileStreams/SpreadsheetFiles.java
@@ -2,29 +2,34 @@ package pw.qxczv.TextExcel.FileStreams;
 import java.util.ArrayList;
 import java.io.*;
 public class SpreadsheetFiles {
-	private static ArrayList<String> FilesFound;
-	private static File directory = new File("bla.text");
+	private static String[] FilesFound;
+	private static File directory = new File("/SavedSpreadsheets");
+	
 	public static boolean exists(String name) {
 		try{
 			UpdateFiles();
-			if(FileSearch(name)){
-				
-			}
+			return FileSearch(name);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			//Change when Ship
 			e.printStackTrace();
 		}
 		return false;
 		
 		
 	}
+	
 	private static boolean FileSearch(String name) throws IOException {
+		for(int i = 0; i < FilesFound.length; i ++){
+			if(FilesFound[i].equals(name)){
+				return true;
+			}
+		}
 		return false;
-		
 	}
+	
 	private static void UpdateFiles() throws IOException{
-		if (!directory.exists()) {
-			directory.createNewFile();
+		if (!directory.mkdir()){
+			 FilesFound = directory.list();
 		}
 	}
 
